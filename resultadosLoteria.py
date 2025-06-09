ea98f72308694b3728ff77d533b477f71dd0337b
Tatiana Villamizar <tm8497077@gmail.com>
2025-06-09 12:36:37 -0500

    actualizacion aleatorios

--- resultadosLoteria.py
+++ resultadosLoteria.py
@@ -1,13 +1,14 @@
 import random
 import compraBoletos 
 
+
 def generarAleatoriosganador():
     numganadores = []
     for i in range(6):
-        num = random.randint(1, 99)
+        num = random.randint(0, 9)
         numganadores.append(num)
     compraBoletos.loteria["ganadores"]. append(numganadores)
-    print(f"Numeros ganadores {numganadores}")
+    print(compraBoletos.loteria["ganadores"])
 
 def generarAleatorios():
     
